a6e2ebe99a727eb5d804f4848827fd11a909aecb
Bryon Vandiver <unicdk@gmail.com>
2020-09-17 01:43:17 -0700

    Checkpoint

--- firmware/main.c
+++ firmware/main.c
@@ -72,6 +72,8 @@ static uint8_t m_enc_scan_response_data[BLE_GAP_ADV_SET_DATA_SIZE_MAX];         
 
 /* USB Definitions */
 #define APP_USBD_INTERFACE_KBD   0
+#define APP_USBD_INTERFACE_MOUSE 1
+#define CONFIG_MOUSE_BUTTON_COUNT   8
 
 /**@brief Struct that contains pointers to the encoded advertising data. */
 static ble_gap_adv_data_t m_adv_data =
@@ -406,48 +408,72 @@ static void ble_stack_init(void)
 }
 
 APP_TIMER_DEF(m_repeated_timer_id);
+static void hid_mouse_user_ev_handler(app_usbd_class_inst_t const * p_inst,
+                                      app_usbd_hid_user_event_t event);
 static void hid_kbd_user_ev_handler(app_usbd_class_inst_t const * p_inst,
                                     app_usbd_hid_user_event_t event);
+
+APP_USBD_HID_MOUSE_GLOBAL_DEF(m_app_hid_mouse,
+                              APP_USBD_INTERFACE_MOUSE,
+                              NRF_DRV_USBD_EPIN1,
+                              CONFIG_MOUSE_BUTTON_COUNT,
+                              hid_mouse_user_ev_handler,
+                              APP_USBD_HID_SUBCLASS_BOOT
+);
+
 APP_USBD_HID_KBD_GLOBAL_DEF(m_app_hid_kbd,
                             APP_USBD_INTERFACE_KBD,
                             NRF_DRV_USBD_EPIN2,
                             hid_kbd_user_ev_handler,
-                            APP_USBD_HID_SUBCLASS_BOOT);
-
+                            APP_USBD_HID_SUBCLASS_BOOT
+);
 
 static void repeated_timer_handler(void * p_context)
 {
+    static uint8_t v = 0;
     static uint8_t m = 0;
-    static uint8_t v = 4;
 
-    if (m == 0) {
-        for (int i = 4; i <= 0xFF; i++) {
-            app_usbd_hid_kbd_key_control(&m_app_hid_kbd, i, false);
-        }
-        for (int i = 0; i < 4; i++) {
-            app_usbd_hid_kbd_key_control(&m_app_hid_kbd, v, true);
-            while (true) {
-                switch (++v) {
-                    case 0: case 1: case 2: case 3: // these are error codes
-                    case 6: case 15: // C/M have hotkeys is bad, 
-                    case 57: // capslock
-                    case 62: case 66: case 67: // Function Keys
-                    case 70: case 71: case 72: // Print screen, scroll lock, pause
-                    case 83: // numslock
-                    case 101: // Application / power (power might be fine)
+    while (true) {
+        switch (++v) {
+            case 0: case 1: case 2: case 3: // these are error codes
+            case 6: case 15: // C/M have hotkeys is bad, 
+            case 57: // capslock
+            case 62: case 66: case 67: // Function Keys
+            case 70: case 71: case 72: // Print screen, scroll lock, pause
+            case 83: // numslock
+            case 101: // Application / power (power might be fine)
 
-                    // Modifiers
-                    case 224: case 225: case 226: case 227:
-                    case 228: case 229: case 230: case 231:
-                        v++; continue ;
-                }
-                break ;
-            }
+            // Modifiers
+            case 224: case 225: case 226: case 227:
+            case 228: case 229: case 230: case 231:
+                v++; continue ;
         }
+        break ;
     }
 
-    app_usbd_hid_kbd_modifier_state_set(&m_app_hid_kbd, 0xFF, false);
-    app_usbd_hid_kbd_modifier_state_set(&m_app_hid_kbd, m++, true);
+    for (int i = 4; i <= 0xFF; i++) {
+        app_usbd_hid_kbd_key_control(&m_app_hid_kbd, i, false);
+    }
+    app_usbd_hid_kbd_key_control(&m_app_hid_kbd, v, true);
+
+    //app_usbd_hid_kbd_modifier_state_set(&m_app_hid_kbd, 0xFF, false);
+    //app_usbd_hid_kbd_modifier_state_set(&m_app_hid_kbd, m, true);
+
+    // update our mouse
+    /*
+    static bool toggle = true;
+
+    for(int i=0; i < CONFIG_MOUSE_BUTTON_COUNT; i++) {
+        app_usbd_hid_mouse_button_state(&m_app_hid_mouse, i, (m >> i) & 1);
+    }
+    
+    app_usbd_hid_mouse_x_move(&m_app_hid_mouse, toggle ? 127 : -127);
+    app_usbd_hid_mouse_y_move(&m_app_hid_mouse, toggle ? 127 : -127);
+    app_usbd_hid_mouse_scroll_move(&m_app_hid_mouse, toggle ? 127 : -127);
+    
+    toggle = !toggle;
+    */
+    m++;
 }
 
 static void hid_kbd_user_ev_handler(app_usbd_class_inst_t const * p_inst,
@@ -458,13 +484,32 @@ static void hid_kbd_user_ev_handler(app_usbd_class_inst_t const * p_inst,
         case APP_USBD_HID_USER_EVT_OUT_REPORT_READY:
             break;
         case APP_USBD_HID_USER_EVT_IN_REPORT_DONE:
-            repeated_timer_handler(NULL);
             break;
         case APP_USBD_HID_USER_EVT_SET_BOOT_PROTO:
             UNUSED_RETURN_VALUE(hid_kbd_clear_buffer(p_inst));
             break;
         case APP_USBD_HID_USER_EVT_SET_REPORT_PROTO:
             UNUSED_RETURN_VALUE(hid_kbd_clear_buffer(p_inst));
+            break;
+        default:
+            break;
+    }
+}
+
+static void hid_mouse_user_ev_handler(app_usbd_class_inst_t const * p_inst,
+                                      app_usbd_hid_user_event_t event)
+{
+    UNUSED_PARAMETER(p_inst);
+    switch (event) {
+        case APP_USBD_HID_USER_EVT_OUT_REPORT_READY:
+            break;
+        case APP_USBD_HID_USER_EVT_IN_REPORT_DONE:
+            break;
+        case APP_USBD_HID_USER_EVT_SET_BOOT_PROTO:
+            UNUSED_RETURN_VALUE(hid_mouse_clear_buffer(p_inst));
+            break;
+        case APP_USBD_HID_USER_EVT_SET_REPORT_PROTO:
+            UNUSED_RETURN_VALUE(hid_mouse_clear_buffer(p_inst));
             break;
         default:
             break;
@@ -508,10 +553,16 @@ static void usb_stack_init()
     err_code = app_usbd_class_append(class_inst_kbd);
     APP_ERROR_CHECK(err_code);
     
+    /*
+    app_usbd_class_inst_t const * class_inst_mouse;
+    class_inst_mouse = app_usbd_hid_mouse_class_inst_get(&m_app_hid_mouse);
+    err_code = app_usbd_class_append(class_inst_mouse);
+    APP_ERROR_CHECK(err_code);
+    */
+
     app_usbd_enable();
     app_usbd_start();
 }
-
 
 int main(void)
 {
@@ -539,7 +590,7 @@ int main(void)
                                 APP_TIMER_MODE_SINGLE_SHOT,
                                 repeated_timer_handler);
 
-    app_timer_start(m_repeated_timer_id, APP_TIMER_TICKS(5000), NULL);
+    app_timer_start(m_repeated_timer_id, APP_TIMER_TICKS(200), NULL);
 
     // Enter main loop.
     for (;;)
